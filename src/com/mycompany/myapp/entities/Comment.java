3605d793040a3e5157d8c853221f6e160313c1d8
maelkallel <mael.kallel@hotmail.fr>
2020-06-17 15:07:27 +0100

    Update Comment.java

--- src/com/mycompany/myapp/entities/Comment.java
+++ src/com/mycompany/myapp/entities/Comment.java
@@ -13,6 +13,44 @@ public class Comment {
     private int id_comment,id_news,id_user;
     private String text;
     private String CreatedAt;
+    private String UserPhoto;
+    private String NbrCmt;
+     private String UserName;
+
+    public Comment(int id_comment, int id_news, int id_user, String text, String CreatedAt, String UserPhoto, String NbrCmt, String UserName) {
+        this.id_comment = id_comment;
+        this.id_news = id_news;
+        this.id_user = id_user;
+        this.text = text;
+        this.CreatedAt = CreatedAt;
+        this.UserPhoto = UserPhoto;
+        this.NbrCmt = NbrCmt;
+        this.UserName = UserName;
+    }
+
+    public String getUserPhoto() {
+        return UserPhoto;
+    }
+
+    public void setUserPhoto(String UserPhoto) {
+        this.UserPhoto = UserPhoto;
+    }
+
+    public String getNbrCmt() {
+        return NbrCmt;
+    }
+
+    public void setNbrCmt(String NbrCmt) {
+        this.NbrCmt = NbrCmt;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
 
     public Comment(String text) {
         this.text = text;
@@ -29,11 +67,7 @@ public class Comment {
     public Comment() {
     }
 
-    @Override
-    public String toString() {
-        return "Comment{" + "id_comment=" + id_comment + ", id_news=" + id_news + ", id_user=" + id_user + ", text=" + text + '}';
-    }
-
+  
     public int getId_comment() {
         return id_comment;
     }
@@ -65,4 +99,11 @@ public class Comment {
     public void setText(String text) {
         this.text = text;
     }
+
+    @Override
+    public String toString() {
+        return "Comment{" + "id_comment=" + id_comment + ", id_news=" + id_news + ", id_user=" + id_user + ", text=" + text + ", CreatedAt=" + CreatedAt + ", UserPhoto=" + UserPhoto + ", NbrCmt=" + NbrCmt + ", UserName=" + UserName + '}';
+    }
+    
+    
 }
